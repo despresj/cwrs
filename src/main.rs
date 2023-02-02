a1d0f68eff673dd109179f756995d22c04e06564
despresj <54477023+despresj@users.noreply.github.com>
2023-02-02 07:12:17 -0500

    start fn

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,33 @@
-struct Solution {
-    common: String,
-}
-
-impl Solution {
-    pub fn gcd_of_strings(str1: String, str2: String) -> String {
-        let mut common_chars = String::new();
-        for c in str1.chars() {
-            for (j, k) in str2.chars().enumerate() {
-                println!("i = {}, c = {}", i, c);
-                println!("j = {}, k = {}", j, k);
-                if c == k {
-                    common_chars.push(c);
-                } else {
-                    println!("else");
-                }
-            }
-        }
-        common_chars
-    }
-}
-
 fn main() {
-    let str1 = String::from("ABCABC");
-    let str2 = String::from("ABC");
-    let solution = Solution {
-        common: Solution::gcd_of_strings(str1, str2),
-    };
-    println!("{}", solution.common);
+    // https://www.codewars.com/kata/51675d17e0c1bed195000001/train/rust
+    //
+    pub fn largest_five_digit_number(num: &str) -> u32 {
+        let mut array: Vec<char> = vec![];
+        for char in num.chars() {
+            array.push(char);
+        }
+        array.sort_unstable();
+        array.reverse();
+        let mut append = String::new();
+        for number in array {
+            append.push(number);
+        }
+
+        let result: u32 = append.parse().unwrap();
+        result
+    }
+    let num = "199991";
+    let output = largest_five_digit_number(num);
+    println!("{}", output);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_basic() {
+        assert_eq!(largest_five_digit_number(&"1234567890"), 67890);
+        assert_eq!(largest_five_digit_number(&"731674765"), 74765);
+    }
 }
