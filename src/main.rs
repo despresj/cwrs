e7f0427f40860a05a310c5cb47d74c5c67d58413
despresj <54477023+despresj@users.noreply.github.com>
2023-02-02 20:21:14 -0500

    prep tomorrow challenge

--- src/main.rs
+++ src/main.rs
@@ -62,9 +62,9 @@ fn largest_five_digit_number(num: &str) -> u32 {
 // "Skippy" --> "##ippy"
 //
 // "Nananananananananananananananana Batman!"
-// -->
 // "####################################man!"
 
+// https://www.codewars.com/kata/5412509bd436bd33920011bc/train/rust
 fn maskify(cc: &str) -> String {
     panic!("{}Implement a rusty solution here!", cc);
 }
