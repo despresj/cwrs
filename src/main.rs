3cc7d4bf3ccd3c40718dc2fe407ff6ec29a09088
despresj <54477023+despresj@users.noreply.github.com>
2023-02-03 07:12:40 -0500

    finished dev by 5

--- src/main.rs
+++ src/main.rs
@@ -32,14 +32,14 @@ pub fn largest_five_digit_number_possible(num: &str) -> u32 {
 //
 fn largest_five_digit_number(num: &str) -> u32 {
     let mut numeric_array: Vec<u32> = vec![];
-    for (i, _) in num.chars().enumerate() {
+    for (n, _) in num.chars().enumerate() {
         let to_slice = num.clone();
 
-        if i + 5 > to_slice.len() {
+        if n + 5 > to_slice.len() {
             break;
         }
 
-        let digits: u32 = to_slice[i..i + 5].parse().unwrap();
+        let digits: u32 = to_slice[n..n + 5].parse().unwrap();
         numeric_array.push(digits);
     }
     let output = numeric_array.iter().max().unwrap();
@@ -75,33 +75,97 @@ fn maskify(cc: &str) -> String {
     format!("{}{}", "#".repeat(&length - 4), &cc[length - 4..length]).to_string()
 }
 
+// Given an integer as input, can you round it to the next
+// (meaning, "greater than or equal") multiple of 5?
+//
+// Examples:
+//
+// input:    output:
+// 0    ->   0
+// 2    ->   5
+// 3    ->   5
+// 12   ->   15
+// 21   ->   25
+// 30   ->   30
+// -2   ->   0
+// -5   ->   -5
+// etc.
+// Input may be any positive or negative integer (including 0).
+//
+// You can assume that all inputs are valid integers.
+fn round_to_next_5(n: i32) -> i32 {
+    if n % 5 == 0 {
+        return n;
+    }
+
+    if n > 0 {
+        println!("n = {}, res = {}", n, n - n % 5 + 5);
+
+        return n - (n % 5) + 5;
+    } else {
+        println!("n = {}, res = {}", n, n - n % 5);
+        return n - n % 5;
+    }
+}
+
 fn main() {
-    let masked = maskify("buttsex");
+    let masked = round_to_next_5(5);
     println!("{}", masked);
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
     #[test]
-    fn it_masks_example_strings() {
-        assert_eq!(maskify("4556364607935616"), "############5616");
-        assert_eq!(maskify("5616"), "5616");
-        assert_eq!(maskify("1"), "1");
-        assert_eq!(maskify("11111"), "#1111");
-    }
-    #[test]
-    fn test_basic_cooler() {
-        assert_eq!(largest_five_digit_number_possible(&"1234567890"), 98765);
-        assert_eq!(largest_five_digit_number_possible(&"48894134"), 98844);
-        assert_eq!(largest_five_digit_number_possible(&"1199991991"), 99999);
-        assert_eq!(largest_five_digit_number_possible(&"731674765"), 77766);
+    fn test_basic_round() {
+        assert_eq!(round_to_next_5(1), 5);
     }
 
     #[test]
-    fn test_basic() {
-        assert_eq!(largest_five_digit_number(&"1234567890"), 67890);
-        assert_eq!(largest_five_digit_number(&"731674765"), 74765);
+    fn test_basic_neg() {
+        assert_eq!(round_to_next_5(-1), 0);
     }
+
+    #[test]
+    fn test_extended() {
+        let tests = [
+            [0, 0],
+            [1, 5],
+            [-1, 0],
+            [-5, -5],
+            [3, 5],
+            [5, 5],
+            [7, 10],
+            [20, 20],
+            [39, 40],
+            [990, 990],
+            [121, 125],
+            [555, 555],
+        ];
+
+        for [x, out] in tests.iter() {
+            assert_eq!(round_to_next_5(*x), *out);
+        }
+    }
+}
+
+#[test]
+fn it_masks_example_strings() {
+    assert_eq!(maskify("4556364607935616"), "############5616");
+    assert_eq!(maskify("5616"), "5616");
+    assert_eq!(maskify("1"), "1");
+    assert_eq!(maskify("11111"), "#1111");
+}
+#[test]
+fn test_basic_cooler() {
+    assert_eq!(largest_five_digit_number_possible(&"1234567890"), 98765);
+    assert_eq!(largest_five_digit_number_possible(&"48894134"), 98844);
+    assert_eq!(largest_five_digit_number_possible(&"1199991991"), 99999);
+    assert_eq!(largest_five_digit_number_possible(&"731674765"), 77766);
+}
+
+#[test]
+fn test_basic() {
+    assert_eq!(largest_five_digit_number(&"1234567890"), 67890);
+    assert_eq!(largest_five_digit_number(&"731674765"), 74765);
 }
