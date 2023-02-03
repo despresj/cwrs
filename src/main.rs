78a9bc107f649ee33bd614709e000a7872cd0af4
despresj <54477023+despresj@users.noreply.github.com>
2023-02-02 19:32:13 -0500

    almost done want to try one thing

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,22 @@
+pub fn largest_five_digit_number(num: &str) -> u32 {
+    let mut char_array: Vec<char> = vec![];
+    for char in num.chars() {
+        char_array.push(char);
+    }
+    char_array.sort_unstable();
+    char_array.reverse();
+    let mut string_of_char_nums = String::new();
+    for number in char_array {
+        string_of_char_nums.push(number);
+    }
+
+    let sub = String::from(&string_of_char_nums[0..5]);
+    let result: u32 = sub.parse().unwrap();
+
+    result
+}
 fn main() {
     // https://www.codewars.com/kata/51675d17e0c1bed195000001/train/rust
-    //
-    pub fn largest_five_digit_number(num: &str) -> u32 {
-        let mut array: Vec<char> = vec![];
-        for char in num.chars() {
-            array.push(char);
-        }
-        array.sort_unstable();
-        array.reverse();
-        let mut append = String::new();
-        for number in array {
-            append.push(number);
-        }
-
-        let result: u32 = append.parse().unwrap();
-        result
-    }
     let num = "199991";
     let output = largest_five_digit_number(num);
     println!("{}", output);
