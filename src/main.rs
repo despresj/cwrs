9c0fa40c0b009ee4e9129c1d1ac3beeed8a8c7fe
despresj <54477023+despresj@users.noreply.github.com>
2023-02-12 15:35:47 -0500

    finish fn

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,30 @@
 #![allow(dead_code)]
 #![allow(unused_variables)]
 mod cws;
+pub fn get_middle(s: &str) -> &str {
+    dbg!(s.len());
+    let n_chars = s.len();
+
+    if &n_chars % 2 == 0 {
+        let half = n_chars / 2;
+        output = &s[half - 1..half + 1];
+        return output;
+    } else {
+        let half = n_chars / 2;
+        let output = &s[half..half + 1];
+        return output;
+    }
+}
 
 fn main() {
-    println!("result = {:?}", cws::find_outlier(&[2, 9, 5, 6]));
+    println!("result = {:?}", get_middle("strippers"));
+}
+
+#[test]
+fn get_middle_test() {
+    assert_eq!(get_middle("test"), "es");
+    assert_eq!(get_middle("testing"), "t");
+    assert_eq!(get_middle("middle"), "dd");
+    assert_eq!(get_middle("A"), "A");
+    assert_eq!(get_middle("of"), "of");
 }
